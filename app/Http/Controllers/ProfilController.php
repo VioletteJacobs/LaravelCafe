f7ddbf892ed8ed0c9c600ef99667823ee69636a4
Violette Jacobs <violettejacobs@MacBook-Pro-de-Violette.local>
2021-01-30 12:56:19 +0100

    test variables

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -7,7 +7,10 @@ use Illuminate\Http\Request;
 class ProfilController extends Controller
 {
     public function index(){
-        return view('welcome');
+        $contenu=[
+            (object)['id'=>1,"titre"=>"Worth Drinking", 'buttonVisit'=>"visit us today", 'promise'=>"Our promise"],
+        ];
+        return view('welcome', compact('contenu'));
     }
     public function about(){
         return view('pages.about');
