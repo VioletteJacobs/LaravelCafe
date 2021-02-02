b7de37d7ea7c5ec694a546998c876558c774cad7
Violette Jacobs <violettejacobs@MacBook-Pro-de-Violette.local>
2021-02-02 11:11:41 +0100

    databasehome

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Home;
 use Illuminate\Http\Request;
 
 class ProfilController extends Controller
 {
     public function index(){
-        $contenu=[
-            (object)['titre'=>"Worth Drinking", 'sousTitre'=> "Fresh coffee", 'para'=> "Every cup of our quality artisan coffee starts with locally sourced, hand picked ingredients. Once you try it, our coffee will be a blissful addition to your everyday morning routine - we guarantee it!", 'buttonVisit'=> "visit us today"],
-            (object)['titre'=>"to you", 'sousTitre'=> "Our promise", 'para'=>"When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!" ]
-        ];
-        return view('welcome', compact('contenu'));
+        $database1= Home:: all();
+        // $contenu=[
+        //     (object)['titre'=>"Worth Drinking", 'sousTitre'=> "Fresh coffee", 'para'=> "Every cup of our quality artisan coffee starts with locally sourced, hand picked ingredients. Once you try it, our coffee will be a blissful addition to your everyday morning routine - we guarantee it!", 'buttonVisit'=> "visit us today"],
+        //     (object)['titre'=>"to you", 'sousTitre'=> "Our promise", 'para'=>"When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!" ]
+        // ];
+        return view('welcome', compact("database1"));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,13 @@
         <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="img/intro.jpg" alt="">
         <div class="intro-text left-0 text-center bg-faded p-5 rounded">
           <h2 class="section-heading mb-4">
-            <span class="section-heading-upper">{{$contenu[0] -> sousTitre}}</span>
-            <span class="section-heading-lower">{{$contenu[0] -> titre}}</span>
+            <span class="section-heading-upper">{{$database1[0] -> sousTitre}}</span>
+            <span class="section-heading-lower">{{$database1[0] -> titre}}</span>
           </h2>
-          <p class="mb-3">{{$contenu[0] -> para}}
+          <p class="mb-3">{{$database1[0] -> para}}
           </p>
           <div class="intro-button mx-auto">
-            <a class="btn btn-primary btn-xl" href="#">{{$contenu[0] -> buttonVisit}}</a>
+            <a class="btn btn-primary btn-xl" href="#">{{$database1[0] -> buttonVisit}}</a>
           </div>
         </div>
       </div>
@@ -26,10 +26,10 @@
           <div class="col-xl-9 mx-auto">
             <div class="cta-inner text-center rounded">
               <h2 class="section-heading mb-4">
-                <span class="section-heading-upper">{{$contenu[1] -> sousTitre}}</span>
-                <span class="section-heading-lower">{{$contenu[1] -> titre}}</span>
+                <span class="section-heading-upper">{{$database1[1] -> sousTitre}}</span>
+                <span class="section-heading-lower">{{$database1[1] -> titre}}</span>
               </h2>
-              <p class="mb-0">{{ $contenu[1] -> para}}</p>
+              <p class="mb-0">{{ $database1[1] -> para}}</p>
             </div>
           </div>
         </div>
